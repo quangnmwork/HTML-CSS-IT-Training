a453bc7cc4e01d00adeb65b4f595aa2c74201774
Quang Nguyen <quangnm.work@gmail.com>
2022-07-14 22:59:29 +0700

    update navbar

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const toggleButton = document.querySelector(".toggle-btn");
 const toggleButtonLine = document.querySelectorAll(".toggle-btn__line");
+const navbar = document.querySelector(".navbar-area");
+const navBarMenu = document.querySelector(".navbar-menu");
 
 toggleButton.addEventListener("click", (e) => {
   e.preventDefault();
@@ -7,4 +9,14 @@ toggleButton.addEventListener("click", (e) => {
   toggleButtonLine[0].classList.toggle("toggle-btn__line-1");
   toggleButtonLine[1].classList.toggle("toggle-btn__line-2");
   toggleButtonLine[2].classList.toggle("toggle-btn__line-3");
+  navBarMenu.classList.toggle("show");
+});
+window.addEventListener("scroll", function (event) {
+  let top = this.scrollY;
+
+  if (top > 100) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
 });
