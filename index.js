5e71f427365122c2a614143f4d9c3b9bf21499cf
Quang Nguyen <quangnm.work@gmail.com>
2022-07-15 11:10:27 +0700

    add accordion,tab

--- index.js
+++ index.js
@@ -2,7 +2,24 @@ const toggleButton = document.querySelector(".toggle-btn");
 const toggleButtonLine = document.querySelectorAll(".toggle-btn__line");
 const navbar = document.querySelector(".navbar-area");
 const navBarMenu = document.querySelector(".navbar-menu");
+const tabs = document.querySelectorAll(".tab-link");
+const tabContents = document.querySelectorAll(".tab-content");
+const accordions = document.querySelectorAll(".accordion");
 
+const resetTabs = (idTab) => {
+  tabs.forEach((tab) => {
+    if (tab.dataset.value !== idTab) tab.classList.remove("active");
+    else tab.classList.add("active");
+  });
+};
+const resetTabContents = (idContent) => {
+  tabContents.forEach((tabContent) => {
+    if (tabContent.id !== idContent) tabContent.style.display = "none";
+    else tabContent.style.display = "flex";
+  });
+};
+resetTabs("introduction");
+resetTabContents("introduction");
 toggleButton.addEventListener("click", (e) => {
   e.preventDefault();
 
@@ -20,3 +37,23 @@ window.addEventListener("scroll", function (event) {
     navbar.classList.remove("sticky");
   }
 });
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", (e) => {
+    e.preventDefault();
+    resetTabs(tab.dataset.value);
+    resetTabContents(tab.dataset.value);
+  });
+});
+
+accordions.forEach((acc) =>
+  acc.addEventListener("click", () => {
+    acc.classList.toggle("active");
+    const panel = acc.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  })
+);
